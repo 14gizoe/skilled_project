7b75e324e610eaa35e4b95f196f4cd7c2ce9d601
yiyaaa <thdtjs1219@gmail.com>
2024-03-17 15:54:40 +0900

    feat: cicd 환경 테스트

--- src/main/java/com/project/skilled_project/TestController.java
+++ src/main/java/com/project/skilled_project/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "perfect running!";
+        return "14기조 cicd 완성이요~~";
     }
 }
