6f6fbb6ae7dde5b85b10c3a3a17dce54197123cb
yiyaaa <thdtjs1219@gmail.com>
2024-03-17 13:39:59 +0900

    feat: git init

---
+++ src/main/java/com/project/skilled_project/SkilledProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.skilled_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilledProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkilledProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/skilled_project/SkilledProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.skilled_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilledProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
