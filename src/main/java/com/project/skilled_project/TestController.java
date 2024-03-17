a01e5e3e311f85fa617a18738fd9cc3eb8720604
yiyaaa <thdtjs1219@gmail.com>
2024-03-17 23:22:05 +0900

    feat: cicd settings

--- src/main/java/com/project/skilled_project/TestController.java
+++ src/main/java/com/project/skilled_project/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "14기조 cicd 완성이요~~ 5트";
+        return "14기조 cicd 완성이요~~";
     }
 }
