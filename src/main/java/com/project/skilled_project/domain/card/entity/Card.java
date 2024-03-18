ae81f267f8f642f246485d0861b964f35f36e37d
devJWL <jwlee4450@gmail.com>
2024-03-18 20:56:47 +0900

    feat(Card): 기본 패키징 구성

---
+++ src/main/java/com/project/skilled_project/domain/card/entity/Card.java
@@ -0,0 +1,31 @@
+package com.project.skilled_project.domain.card.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Card {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(nullable = false)
+  private String title;
+  @Column(nullable = false)
+  private String description;
+  private LocalDateTime deadline;
+  @Column(nullable = false)
+  private Long columnId;
+  @Column(nullable = false)
+  private String color = "#FFFFFF";
+
+}
---
+++ src/main/java/com/project/skilled_project/domain/card/repository/CardQuerydslImpl.java
@@ -0,0 +1,10 @@
+package com.project.skilled_project.domain.card.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CardQuerydslImpl implements CardQuerydsl{
+  private final JPAQueryFactory jpaQueryFactory;
+
+}
