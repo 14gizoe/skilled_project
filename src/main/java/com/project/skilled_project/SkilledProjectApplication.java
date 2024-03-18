4738026f55dc4962647ad965d3b66c00c131e35c
yiyaaa <thdtjs1219@gmail.com>
2024-03-18 19:21:41 +0900

    feat: 코드 컨벤션

--- src/main/java/com/project/skilled_project/SkilledProjectApplication.java
+++ src/main/java/com/project/skilled_project/SkilledProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkilledProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SkilledProjectApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SkilledProjectApplication.class, args);
+  }
 
 }
--- src/test/java/com/project/skilled_project/SkilledProjectApplicationTests.java
+++ src/test/java/com/project/skilled_project/SkilledProjectApplicationTests.java
@@ -6,9 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SkilledProjectApplicationTests {
 
-    @Test
-    void contextLoads() {
-        System.out.println("hello");
-    }
+  @Test
+  void contextLoads() {
+    System.out.println("hello");
+  }
 
 }
