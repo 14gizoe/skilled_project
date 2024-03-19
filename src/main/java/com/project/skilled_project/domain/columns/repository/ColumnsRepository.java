68f63b588b9e91d8fc86aaab5e741fac1a789852
mine <mineki94@naver.com>
2024-03-19 11:24:22 +0900

    refactor(Columns): 컬럼 필요없는 파일 삭제, 이름 수정

---
+++ src/main/java/com/project/skilled_project/domain/columns/repository/ColumnsRepository.java
@@ -0,0 +1,8 @@
+package com.project.skilled_project.domain.columns.repository;
+
+import com.project.skilled_project.domain.columns.entity.Columns;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ColumnsRepository extends JpaRepository<Columns, Long> {
+
+}
