26b916070ee4e887ebca74f636dfb7121cf5c66b
Hu6r1s <darmaa333@gmail.com>
2024-03-19 15:45:08 +0900

    feat(User): 유저 회원가입 기능 구현

--- src/main/java/com/project/skilled_project/domain/user/controller/UserController.java
+++ src/main/java/com/project/skilled_project/domain/user/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.project.skilled_project.domain.user.controller;
 
+import com.project.skilled_project.domain.user.dto.SignupRequestDto;
 import com.project.skilled_project.domain.user.service.UserService;
 import com.project.skilled_project.domain.user.service.UserServiceImpl;
+import com.project.skilled_project.global.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   private final UserService userService;
+
+  @PostMapping("/signup")
+  public ResponseEntity<CommonResponse<Void>> signup(@RequestBody SignupRequestDto signupRequest) {
+    userService.signup(signupRequest.getEmail(), signupRequest.getUsername(),
+        signupRequest.getPassword());
+    return CommonResponse.ok(null);
+  }
 }
--- src/main/java/com/project/skilled_project/domain/user/service/UserService.java
+++ src/main/java/com/project/skilled_project/domain/user/service/UserService.java
@@ -2,4 +2,5 @@ package com.project.skilled_project.domain.user.service;
 
 public interface UserService {
 
+  void signup(String email, String username, String password);
 }
