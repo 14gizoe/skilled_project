706785d2a773a185569a7c07e9f4ba81fdb7f318
Hu6r1s <darmaa333@gmail.com>
2024-03-20 10:16:14 +0900

    feat(Comment): 댓글 수정 기능 구현

---
+++ src/main/java/com/project/skilled_project/domain/comment/dto/CommentUpdateRequestDto.java
@@ -0,0 +1,13 @@
+package com.project.skilled_project.domain.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentUpdateRequestDto {
+
+  private String content;
+}
--- src/main/java/com/project/skilled_project/domain/comment/service/CommentService.java
+++ src/main/java/com/project/skilled_project/domain/comment/service/CommentService.java
@@ -1,7 +1,9 @@
 package com.project.skilled_project.domain.comment.service;
 
 
-public interface CommentSerivce {
+public interface CommentService {
 
   void createComment(String content, Long cardId, String verifiedUser);
+
+  void updateComment(Long commentId, String content, String username);
 }
