e83a0d4ecf4129daeced28db602119350f7bd42c
Juno <88548682+devJWL@users.noreply.github.com>
2024-03-21 12:09:46 +0900

    feat(worker): 카드에서 접근하기 위한 매소드 구현 (#19)

--- src/main/java/com/project/skilled_project/domain/worker/service/WorkerService.java
+++ src/main/java/com/project/skilled_project/domain/worker/service/WorkerService.java
@@ -1,10 +1,12 @@
 package com.project.skilled_project.domain.worker.service;
 
 import com.project.skilled_project.domain.worker.dto.WorkerRequestDto;
+import java.util.List;
 
 public interface WorkerService {
 
   void addWorkers(WorkerRequestDto workerRequestDto);
-
   void deleteWorkers(WorkerRequestDto workerRequestDto);
+
+  List<Long> findAllUserIdByCardId(Long cardId);
 }
