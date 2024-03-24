788b597c79b0497a2b955c0f194e81e64179a5e6
yiyaaa <thdtjs1219@gmail.com>
2024-03-24 23:35:51 +0900

    feat: test code 주석

--- src/test/java/com/project/skilled_project/SkilledProjectApplicationTests.java
+++ src/test/java/com/project/skilled_project/SkilledProjectApplicationTests.java
@@ -1,14 +1,14 @@
-package com.project.skilled_project;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SkilledProjectApplicationTests {
-
-  @Test
-  void contextLoads() {
-    System.out.println("hello");
-  }
-
-}
+//package com.project.skilled_project;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SkilledProjectApplicationTests {
+//
+//  @Test
+//  void contextLoads() {
+//    System.out.println("hello");
+//  }
+//
+//}
